8fa815d17d259a33b93bab583f29ed8f1f934225
LapisSea <toni.tube.you@gmail.com>
2018-01-30 11:55:12 +0100

    inital

---
+++ src/com/lapissea/glfwwin/GlfwMonitor.java
@@ -0,0 +1,282 @@
+package com.lapissea.glfwwin;
+
+import com.lapissea.util.PairM;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.glfw.GLFWMonitorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+
+import java.awt.geom.Rectangle2D;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public class GlfwMonitor{
+	
+	public static final class Rect extends Rectangle2D{
+		
+		public final int x, y, width, height;
+		
+		public Rect(int x, int y, int w, int h){
+			this.x=x;
+			this.y=y;
+			width=w;
+			height=h;
+		}
+		
+		@Override
+		public double getX(){
+			return (double)x;
+		}
+		
+		@Override
+		public double getY(){
+			return (double)y;
+		}
+		
+		@Override
+		public double getWidth(){
+			return (double)width;
+		}
+		
+		@Override
+		public double getHeight(){
+			return (double)height;
+		}
+		
+		@Override
+		public boolean isEmpty(){
+			return (width<=0.0f)||(height<=0.0f);
+		}
+		
+		
+		@Override
+		public void setRect(double x, double y, double w, double h){
+			throw new UnsupportedOperationException();
+		}
+		
+		@Override
+		public int outcode(double x, double y){
+			int out=0;
+			if(width<=0){
+				out|=OUT_LEFT|OUT_RIGHT;
+			}else if(x<this.x){
+				out|=OUT_LEFT;
+			}else if(x>this.x+(double)width){
+				out|=OUT_RIGHT;
+			}
+			if(height<=0){
+				out|=OUT_TOP|OUT_BOTTOM;
+			}else if(y<this.y){
+				out|=OUT_TOP;
+			}else if(y>this.y+(double)height){
+				out|=OUT_BOTTOM;
+			}
+			return out;
+		}
+		
+		@Override
+		public Rectangle2D getBounds2D(){
+			return new Float(x, y, width, height);
+		}
+		
+		@Override
+		public Rectangle2D createIntersection(Rectangle2D r){
+			Rectangle2D dest;
+			if(r instanceof Float){
+				dest=new Rectangle2D.Float();
+			}else{
+				dest=new Rectangle2D.Double();
+			}
+			Rectangle2D.intersect(this, r, dest);
+			return dest;
+		}
+		
+		@Override
+		public Rectangle2D createUnion(Rectangle2D r){
+			Rectangle2D dest;
+			if(r instanceof Float){
+				dest=new Rectangle2D.Float();
+			}else{
+				dest=new Rectangle2D.Double();
+			}
+			Rectangle2D.union(this, r, dest);
+			return dest;
+		}
+		
+		@Override
+		public String toString(){
+			return getClass().getName()
+			       +"[x="+x+
+			       ",y="+y+
+			       ",w="+width+
+			       ",h="+height+"]";
+		}
+	}
+	
+	private static boolean INITED;
+	
+	private static       GlfwMonitor       PRIMARY_MONITOR =null;
+	private static final List<GlfwMonitor> MONITORS        =new ArrayList<>(1);
+	private static final List<GlfwMonitor> MONITORS_F      =Collections.unmodifiableList(MONITORS);
+	private static final List<Rect>        MONITOR_GROUPS  =new ArrayList<>(1);
+	private static final List<Rect>        MONITOR_GROUPS_F=Collections.unmodifiableList(MONITOR_GROUPS);
+	
+	public static void init(){
+		if(INITED) return;
+		INITED=true;
+		
+		glfwInit();
+		update();
+		glfwSetMonitorCallback(GLFWMonitorCallback.create((monitor, event)->update()));
+	}
+	
+	private static void check(){
+		if(!INITED) throw new IllegalStateException("\"init\" FUNCTION WAS NOT CALLED");
+	}
+	
+	private static void update(){
+		
+		MONITORS.clear();
+		MONITOR_GROUPS.clear();
+		
+		PointerBuffer monitors=glfwGetMonitors();
+		for(int i=0;i<monitors.capacity();i++){
+			MONITORS.add(new GlfwMonitor(monitors.get(i)));
+		}
+		
+		List<Rectangle2D> groups=new ArrayList<>(1);
+		
+		List<Rectangle2D> all=new ArrayList<>(1);
+		all.addAll(MONITORS.stream().map(a->a.bounds).collect(Collectors.toList()));
+		for(GlfwMonitor m : MONITORS){
+			
+			Rectangle2D group =new Rectangle2D.Float(m.bounds.x, m.bounds.y, m.bounds.width, m.bounds.height);
+			boolean     change=true;
+			while(change){
+				change=false;
+				
+				for(Rectangle2D monitor : all){
+					if(monitor.equals(group)) continue;
+					
+					boolean yFlush=group.getMaxY()==monitor.getMaxY()&&group.getMinY()==monitor.getMinY();
+					if(yFlush){
+						boolean rightCon=group.getMaxX()==monitor.getMinX();
+						boolean leftCon =monitor.getMaxX()==group.getMinX();
+						if(rightCon||leftCon){
+							change=true;
+							group=group.createUnion(monitor);
+							
+						}
+					}
+					
+					boolean xFlush=group.getMaxX()==monitor.getMaxX()&&group.getMinX()==monitor.getMinX();
+					if(xFlush){
+						boolean topCon   =group.getMaxY()==monitor.getMinY();
+						boolean bottomCon=monitor.getMaxY()==group.getMinY();
+						if(topCon||bottomCon){
+							change=true;
+							group=group.createUnion(monitor);
+						}
+					}
+					
+				}
+			}
+			
+			if(!groups.contains(group)) groups.add(group);
+			if(!all.contains(group)) all.add(group);
+			
+		}
+		groups.stream().map(g->new Rect((int)g.getX(), (int)g.getY(), (int)g.getWidth(), (int)g.getHeight())).forEach(MONITOR_GROUPS::add);
+		long ph=glfwGetPrimaryMonitor();
+		PRIMARY_MONITOR=MONITORS.stream().filter(m->m.handle==ph).findAny().orElseThrow(()->new RuntimeException("No primary monitor in monitor list?? (bug)"));
+	}
+	
+	public static GlfwMonitor getPrimaryMonitor(){
+		check();
+		return PRIMARY_MONITOR;
+	}
+	
+	public static List<GlfwMonitor> getMonitors(){
+		check();
+		return MONITORS_F;
+	}
+	
+	public static List<Rect> getGroups(){
+		check();
+		return MONITOR_GROUPS_F;
+	}
+	
+	public static boolean moveToVisible(Rectangle2D windowRect){
+		
+		if(getGroups().stream().noneMatch(group->group.contains(windowRect))){
+			
+			Optional<PairM<Rect, Rectangle2D>> r=
+					getGroups().stream()
+					           .map(group->new PairM<>(group, new Rectangle2D.Double(group.getX(), group.getY(), windowRect.getWidth(), windowRect.getHeight()).createIntersection(group)))
+					           .max(Comparator.comparingDouble(a->a.obj2.getWidth()*a.obj2.getHeight()));
+			if(r.isPresent()){
+				Rectangle2D intersect=r.get().obj2;
+				Rect        group    =r.get().obj1;
+				
+				double x, y,
+						w=Math.min(windowRect.getWidth(), group.getWidth()),
+						h=Math.min(windowRect.getHeight(), group.getHeight());
+				
+				if(windowRect.getMinX()<group.getMinX()) x=group.getMinX();
+				else if(windowRect.getMaxX()>group.getMaxX()) x=group.getMaxX()-w;
+				else x=windowRect.getX();
+				
+				if(windowRect.getMinY()<group.getMinY()) y=group.getMinY();
+				else if(windowRect.getMaxY()>group.getMaxY()) y=group.getMaxY()-h;
+				else y=windowRect.getY();
+				
+				windowRect.setRect(x, y, w, h);
+				
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	//////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////
+	
+	final        long handle;
+	public final int  refreshRate;
+	public final Rect bounds;
+	
+	
+	public GlfwMonitor(long handle){
+		this.handle=handle;
+		
+		
+		GLFWVidMode mode=glfwGetVideoMode(handle);
+		
+		refreshRate=mode.refreshRate();
+		
+		int[] xBuf={0}, yBuf={0};
+		glfwGetMonitorPos(handle, xBuf, yBuf);
+		bounds=new Rect(xBuf[0], yBuf[0], mode.width(), mode.height());
+	}
+	
+	@Override
+	public int hashCode(){
+		return bounds.hashCode()+refreshRate;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if(obj==this) return true;
+		if(!(obj instanceof GlfwMonitor)) return false;
+		GlfwMonitor other=(GlfwMonitor)obj;
+		return other.handle==handle;
+	}
+	
+	@Override
+	public String toString(){
+		return "GlfwMonitor{refreshRate="+refreshRate+", bounds="+bounds+"}";
+	}
+	
+}
---
+++ src/com/lapissea/glfwwin/GlfwWindow.java
@@ -0,0 +1,334 @@
+package com.lapissea.glfwwin;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedTreeMap;
+import com.lapissea.util.TextUtil;
+import com.lapissea.util.event.change.ChangeRegistry;
+import com.lapissea.util.event.change.ChangeRegistryBool;
+import com.lapissea.vec.ChangeRegistryVec2i;
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWImage;
+
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class GlfwWindow{
+	
+	protected long handle=NULL;
+	
+	public final ChangeRegistry<String> title=new ChangeRegistry<>("", e->{
+		if(isCreated()) glfwSetWindowTitle(handle, e.object);
+	});
+	
+	public final ChangeRegistryBool maximized=new ChangeRegistryBool(false, e->{
+		if(!isCreated()&&!isFullScreen()) return;
+		if(e.bool) glfwMaximizeWindow(handle);
+		else glfwRestoreWindow(handle);
+	});
+	
+	public final ChangeRegistryBool visible=new ChangeRegistryBool(false, e->{
+		if(!isCreated()&&!isFullScreen()) return;
+		if(e.bool) glfwShowWindow(handle);
+		else glfwHideWindow(handle);
+	});
+	
+	public final ChangeRegistryVec2i size=new ChangeRegistryVec2i(600, 400, e->{
+		if(isCreated()&&!isFullScreen()) glfwSetWindowSize(handle, e.getSource().x(), e.getSource().y());
+	});
+	
+	public final ChangeRegistryVec2i pos=new ChangeRegistryVec2i(-1, -1, e->{
+		if(isCreated()&&!isFullScreen()) glfwSetWindowPos(handle, e.getSource().x(), e.getSource().y());
+	}){
+		
+		@Override
+		public ChangeRegistryVec2i set(int x, int y){
+			
+			if(x==-1&&y==-1){
+				GlfwMonitor monitor=GlfwMonitor.getPrimaryMonitor();
+				
+				super.set((int)monitor.bounds.getCenterX()-size.x()/2, (int)monitor.bounds.getCenterY()-size.y()/2);
+				return this;
+			}
+			
+			super.set(x, y);
+			return this;
+		}
+	};
+	
+	public final ChangeRegistry<GlfwMonitor> monitor=new ChangeRegistry<GlfwMonitor>(e->{
+		if(!isCreated()) return;
+		
+		glfwSetWindowMonitor(handle, e.object.handle, pos.x(), pos.y(), e.object.bounds.width, e.object.bounds.height, e.object.refreshRate);
+	});
+	
+	
+	public GlfwWindow(){
+	}
+	
+	public GlfwWindow init(){
+		return init(true, true);
+	}
+	
+	public GlfwWindow init(boolean resizeable, boolean decorated){
+		
+		pos.set(pos);
+		Rectangle2D windowRect=new Rectangle2D.Float();
+		windowRect.setRect(pos.x(), pos.y(), size.x(), size.y());
+		
+		if(GlfwMonitor.moveToVisible(windowRect)){
+			pos.set((int)windowRect.getX(), (int)windowRect.getY());
+			size.set((int)windowRect.getWidth(), (int)windowRect.getHeight());
+		}
+		
+		preInit(resizeable, decorated);
+		
+		
+		if(isFullScreen()){
+			GlfwMonitor monitor=this.monitor.get();
+			handle=glfwCreateWindow(monitor.bounds.width, monitor.bounds.height, "", monitor.handle, handle);
+		}else handle=glfwCreateWindow(100, 100, "", NULL, handle);
+		
+		initProps();
+		
+		return this;
+	}
+	
+	public boolean isFullScreen(){
+		return monitor.get()!=null;
+	}
+	
+	private void preInit(boolean resizeable, boolean decorated){
+		glfwWindowHint(GLFW_RESIZABLE, resizeable?GLFW_TRUE:GLFW_FALSE);
+		glfwWindowHint(GLFW_DECORATED, decorated?GLFW_TRUE:GLFW_FALSE);
+		if(isFullScreen()) glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+	}
+	
+	private void initProps(){
+		glfwSetWindowTitle(handle, title.get());
+		
+		if(!isFullScreen()){
+			glfwSetWindowPos(handle, pos.x(), pos.y());
+			glfwSetWindowSize(handle, size.x(), size.y());
+			if(maximized.get()) glfwMaximizeWindow(handle);
+			if(visible.get()) glfwShowWindow(handle);
+		}
+		
+		
+		glfwSetWindowMaximizeCallback(handle, (window, maximized)->{
+			if(window!=handle) return;
+			this.maximized.set(maximized);
+		});
+		glfwSetWindowSizeCallback(handle, (window, w, h)->{
+			if(window!=handle) return;
+			size.set(w, h);
+		});
+		glfwSetWindowPosCallback(handle, (window, xpos, ypos)->{
+			if(window!=handle) return;
+			pos.set(xpos, ypos);
+		});
+	}
+	
+	public GlfwWindow setUserPointer(long pointer){
+		glfwSetWindowUserPointer(handle, pointer);
+		return this;
+	}
+	
+	public long getUserPointer(){
+		return glfwGetWindowUserPointer(handle);
+	}
+	
+	public void setIcon(GLFWImage... images){
+		if(images.length==0) return;
+		
+		try(final GLFWImage.Buffer iconSet=GLFWImage.malloc(images.length)){
+			for(int i=images.length-1;i>=0;i--){
+				iconSet.put(i, images[i]);
+			}
+			glfwSetWindowIcon(handle, iconSet);
+		}
+	}
+	
+	public static GLFWImage imgToGlfw(BufferedImage image){
+		
+		ByteBuffer buffer=BufferUtils.createByteBuffer(image.getWidth()*image.getHeight()*4);
+		
+		for(int i=0;i<image.getHeight();i++){
+			for(int j=0;j<image.getWidth();j++){
+				int colorSpace=image.getRGB(j, i);
+				buffer.put((byte)((colorSpace>>16)&0xFF));
+				buffer.put((byte)((colorSpace>>8)&0xFF));
+				buffer.put((byte)((colorSpace>>0)&0xFF));
+				buffer.put((byte)((colorSpace>>24)&0xFF));
+			}
+		}
+		
+		buffer.flip();
+		
+		return GLFWImage.create().set(image.getWidth(), image.getHeight(), buffer);
+	}
+	
+	public boolean isCreated(){
+		return handle!=NULL;
+	}
+	
+	public void pollEvents(){
+		glfwPollEvents();
+	}
+	
+	public boolean shouldClose(){
+		return glfwWindowShouldClose(handle);
+	}
+	
+	
+	public void destroy(){
+		if(!isCreated()) throw new IllegalStateException();
+		glfwDestroyWindow(handle);
+		handle=NULL;
+	}
+	
+	
+	public void hide(){
+		visible.set(false);
+	}
+	
+	public void show(){
+		visible.set(true);
+	}
+	
+	
+	public boolean isHidden(){
+		return !isVisible();
+	}
+	
+	public boolean isVisible(){
+		return visible.get();
+	}
+	
+	
+	//=======SAVE=======//
+	
+	
+	public GlfwWindow loadState(String data){
+		return loadState(new Gson().fromJson(data, HashMap.class));
+	}
+	
+	public GlfwWindow loadState(File file){
+		try(Reader data=new BufferedReader(new FileReader(file))){
+			return loadState(data);
+		}catch(IOException e){}
+		return this;
+	}
+	
+	public GlfwWindow loadState(Reader data){
+		return loadState(new Gson().fromJson(data, HashMap.class));
+	}
+	
+	public GlfwWindow loadState(Map<String, LinkedTreeMap> data){
+		
+		try{
+			IntBuffer target=ByteBuffer.allocate(Integer.SIZE/Byte.SIZE*4)
+			                           .order(ByteOrder.nativeOrder())
+			                           .put(TextUtil.hexStringToByteArray(((Map<String, ?>)data).get("target").toString()))
+			                           .flip()
+			                           .asIntBuffer();
+			
+			Rectangle2D rect=new Rectangle2D.Float(target.get(), target.get(), target.get(), target.get());
+			
+			monitor.set(GlfwMonitor.getMonitors()
+			                       .stream()
+			                       .filter(m->m.bounds.equals(rect))
+			                       .findAny()
+			                       .orElseGet(GlfwMonitor::getPrimaryMonitor));
+			return this;
+		}catch(Throwable e){}
+		
+		try{
+			LinkedTreeMap<String, Number> size=data.get("size");
+			this.size.set(size.getOrDefault("width", this.size.x()).intValue(),
+			              size.getOrDefault("height", this.size.y()).intValue());
+		}catch(Throwable e){}
+		
+		try{
+			LinkedTreeMap<String, Number> pos=data.get("location");
+			this.pos.set(pos.getOrDefault("top", this.pos.x()).intValue(),
+			             pos.getOrDefault("left", this.pos.y()).intValue());
+		}catch(Throwable e){}
+		try{
+			maximized.set(Boolean.parseBoolean(data.get("max")+""));
+		}catch(Throwable e){
+			e.printStackTrace();
+		}
+		
+		
+		return this;
+	}
+	
+	private @NotNull Gson getG(){
+		return new GsonBuilder().setPrettyPrinting()
+		                        .disableHtmlEscaping()
+		                        .create();
+	}
+	
+	public GlfwWindow saveState(File file){
+		try(Writer data=new BufferedWriter(new FileWriter(file))){
+			return saveState(data);
+		}catch(IOException e){}
+		return this;
+	}
+	
+	public GlfwWindow saveState(Writer out){
+		getG().toJson(saveState(new JsonObject()), out);
+		return this;
+	}
+	
+	public String saveState(){
+		return getG().toJson(saveState(new JsonObject()));
+	}
+	
+	public JsonObject saveState(JsonObject json){
+		if(!isCreated()) throw new IllegalStateException();
+		
+		if(isFullScreen()){
+			ByteBuffer bytes=ByteBuffer.allocate(Integer.SIZE/Byte.SIZE*4).order(ByteOrder.nativeOrder());
+			
+			GlfwMonitor m=monitor.get();
+			bytes.putInt(m.bounds.x);
+			bytes.putInt(m.bounds.y);
+			bytes.putInt(m.bounds.width);
+			bytes.putInt(m.bounds.height);
+			
+			json.addProperty("target", TextUtil.bytesToHex(bytes.array()));
+		}else{
+			boolean max=maximized.get();
+			if(max) maximized.set(false);
+			
+			JsonObject size=new JsonObject();
+			size.addProperty("width", this.size.x());
+			size.addProperty("height", this.size.y());
+			JsonObject location=new JsonObject();
+			location.addProperty("top", pos.x());
+			location.addProperty("left", pos.y());
+			
+			if(max) maximized.set(true);
+			
+			json.add("size", size);
+			json.add("location", location);
+			json.addProperty("max", maximized.get());
+		}
+		return json;
+	}
+	
+}
